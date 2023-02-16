a4c000611158883d495bccc5b39c44c12a5028ab
mustapha ibrahim <mustaphaibrahim566@gmail.com>
2023-02-16 10:15:16 -0800

    Hello,puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - A c program that prints a line with put function
+* return: 0 (success)
+/*
+int main(void)
+{
+put("\"Programming is like building a multilingual puzzle");
+return (0);
+}
