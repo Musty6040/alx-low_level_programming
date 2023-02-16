4a243bb291d1bc300709f37fd24d1cd173352085
mustapha ibrahim <mustaphaibrahim566@gmail.com>
2023-02-16 14:51:25 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
-* main - A program that uses puts to print a string
-* return: ends with 0
+* main- A program that print with puts funcion
+* Return: 0 (success)
 */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+puts("\" Programming is like building a multilingual puzzle");
+(return) (0) :
 }
