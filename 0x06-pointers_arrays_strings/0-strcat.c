20fad131dd17a56e11f9f8b629e3c5757c31805b
mustapha ibrahim <mustaphaibrahim566@gmail.com>
2023-03-01 15:23:35 -0800

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcat - a function that links two strings together
+ * @dest: copy to
+ * @src: copy from
+ * Return: two link  strings
+ */
+char *_strcat(char *dest, char *src)
+{
+	int j, n;
+
+	for (j = 0; dest[j] != '\0'; j++)
+	{
+	}
+
+	for (n = 0; (dest[j + n] = *src++) != '\0'; n++)
+	{
+	}
+
+	return (dest);
+}
