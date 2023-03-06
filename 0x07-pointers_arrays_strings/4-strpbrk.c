00cef4b11828d1ef6ed99b9be6845edbf190860d
mustapha ibrahim <mustaphaibrahim566@gmail.com>
2023-03-06 05:17:38 -0800

    strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strpbrk -  a function that searches a string for any of a set of bytes
+ * @s: supply
+ * @accept: input
+ * Return: Always 0 (Success)
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int x, n;
+
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		for (n = 0; accept[n] != '\0'; n++)
+		{
+			if (s[x] == accept[n])
+				return (s + x);
+		}
+	}
+
+	return (NULL);
+}
