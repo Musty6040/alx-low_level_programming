814b9ec3175410d73f3ea7773656957a3167c36a
mustapha ibrahim <mustaphaibrahim566@gmail.com>
2023-04-05 11:05:56 -0700

    Free

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a linked list
+ * @head: pointer to the listint_t list to be freed
+ */
+void free_listint2(listint_t **head)
+{
+listint_t *snode;
+
+while (head)
+
+{
+snode = (*head)->next;
+free(*head);
+*head = snode;
+}
+*head = NULL;
+}
