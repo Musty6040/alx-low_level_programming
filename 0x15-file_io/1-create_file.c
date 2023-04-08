d870a2dec336ba84d9afed2afbae7c22d28e7ff0
mustapha ibrahim <mustaphaibrahim566@gmail.com>
2023-04-08 11:23:45 -0700

    update on the second task

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,14 +13,11 @@ int create_file(const char *filename, char *text_content)
 int fd;
 int nfig = 0;
 int rw;
-
 if (filename == NULL) 
 return (-1);
 if (text_content != NULL) 
-
 while (text_content[nfig])
 nfig++;
-
 fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 rw = write(fd, text_content, nfig);
 if (fd == -1 || rw == -1) 
